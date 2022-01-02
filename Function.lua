e21bc3ed2cfd56bd071103ae04a15a8101c08e90
bossxerr <70553957+Bossxer@users.noreply.github.com>
2022-01-02 17:39:59 -0600

    Update Function.lua

--- Function.lua
+++ Function.lua
@@ -31,3 +31,88 @@ local AntiAfk = function()
         v:Disable()
     end
 end
+
+local VWalkspeed = function(Ammount)
+while _G.EWalkspeed == true do
+   local HRP = game.Players.LocalPlayer.Character.HumanoidRootPart
+   local C = game.Workspace.CurrentCamera
+   local LV = C.CFrame.LookVector
+   for i,v in pairs(UIS:GetKeysPressed()) do
+       if v.KeyCode == Enum.KeyCode.W then
+           W = true
+       end
+       if v.KeyCode == Enum.KeyCode.A then
+           A = true
+       end
+       if v.KeyCode == Enum.KeyCode.S then
+           S = true
+       end
+       if v.KeyCode == Enum.KeyCode.D then
+           D = true
+       end
+   end
+
+   if W == true and S == true then
+       yVelo = false
+       W,S = nil
+   end
+
+   if A == true and D == true then
+       xVelo = false
+       A,D = nil
+   end
+
+   if yVelo ~= false then
+       if W == true then
+           if xVelo ~= false then
+               if A == true then
+                   local LeftLV = (C.CFrame * CFrame.Angles(0, math.rad(45), 0)).LookVector
+                   HRP.Velocity = Vector3.new((LeftLV.X * Ammount), HRP.Velocity.Y, (LeftLV.Z * Ammount))
+                   W,A = nil
+               else
+                   if D == true then
+                       local RightLV = (C.CFrame * CFrame.Angles(0, math.rad(-45), 0)).LookVector
+                       HRP.Velocity = Vector3.new((RightLV.X * Ammount), HRP.Velocity.Y, (RightLV.Z * Ammount))
+                       W,D = nil
+                   end
+               end
+           end
+       else
+           if S == true then
+               if xVelo ~= false then
+                   if A == true then
+                       local LeftLV = (C.CFrame * CFrame.Angles(0, math.rad(135), 0)).LookVector
+                       HRP.Velocity = Vector3.new((LeftLV.X * Ammount), HRP.Velocity.Y, (LeftLV.Z * Ammount))
+                       S,A = nil
+                   else
+                       if D == true then
+                           local RightLV = (C.CFrame * CFrame.Angles(0, math.rad(-135), 0)).LookVector
+                           HRP.Velocity = Vector3.new((RightLV.X * Ammount), HRP.Velocity.Y, (RightLV.Z * Ammount))
+                           S,D = nil
+                       end
+                   end
+               end
+           end
+       end
+   end
+
+   if W == true then
+       HRP.Velocity = Vector3.new((LV.X * Ammount), HRP.Velocity.Y, (LV.Z * Ammount))
+   end
+   if S == true then
+       HRP.Velocity = Vector3.new(-(LV.X * Ammount), HRP.Velocity.Y, -(LV.Z * Ammount))
+   end
+   if A == true then
+       local LeftLV = (C.CFrame * CFrame.Angles(0, math.rad(90), 0)).LookVector
+       HRP.Velocity = Vector3.new((LeftLV.X * Ammount), HRP.Velocity.Y, (LeftLV.Z * Ammount))
+   end
+   if D == true then
+       local RightLV = (C.CFrame * CFrame.Angles(0, math.rad(-90), 0)).LookVector
+       HRP.Velocity = Vector3.new((RightLV.X * Ammount), HRP.Velocity.Y, (RightLV.Z * Ammount))
+   end
+
+   xVelo, yVelo, W, A, S, D = nil
+   wait()
+   end
+end
+
