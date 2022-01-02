ebfb65e21d49e627c833871c2bb922947d67bc2c
bossxerr <70553957+Bossxer@users.noreply.github.com>
2022-01-02 17:42:49 -0600

    Update Function.lua

--- Function.lua
+++ Function.lua
@@ -32,9 +32,16 @@ local NoLag = function()
 end
 
 local AntiAfk = function()
-    for i,v in pairs(getconnections(game.Players.LocalPlayer.Idled)) do
-        v:Disable()
-    end
+	local GC = getconnections or get_signal_cons
+	    if GC then
+		for i,v in pairs(GC(game.Players.LocalPlayer.Idled)) do
+		    if v["Disable"] then
+			v["Disable"](v)
+		    elseif v["Disconnect"] then
+			v["Disconnect"](v)
+		end
+	    end
+	end
 end
 
 local VWalkspeed = function(Ammount)
