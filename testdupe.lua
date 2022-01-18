02e3e3cdae15cde90b88bedc4dc5b9e3f3ac0ec4
bossxerr <70553957+Bossxer@users.noreply.github.com>
2022-01-18 12:10:15 -0600

    Update testdupe.lua

--- testdupe.lua
+++ testdupe.lua
@@ -312,4 +312,4 @@ end
 
 end
 
-syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Bossxer/BobloxTS/main/testdupe.lua'))()")
